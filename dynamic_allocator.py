5ccd12dde3b14afe806f4a87302ec48972187f81
Emacser <roxman98@naver.com>
2021-12-21 23:09:31 +0900

    [UPDATE]: Update basic allocator

--- dynamic_allocator.py
+++ dynamic_allocator.py
@@ -4,43 +4,50 @@ import time
 import re
 import subprocess
 
-class MicroService:
-    def __init__(self, containerName):
-        self.containerName = containerName
+class Container:
+    def __init__(self, name):
+        self.name = name
 
-    def updateStats(self):
-        for line in os.popen("docker stats --no-stream {}".format(self.containerName)).readlines()[1:]:
-            id, name, cpuPercentage, memoryUsage, _, memoryLimit, memoryPercentage, netIn, _, netOut, blockIn, _, blockOut, pids = line.split()
+        self.id = ""
+        self.weight = 1024
+        self.memory = 4
 
-            print("Id: {}".format(id))
-            print("Name: {}".format(name))
-            print("CPU % : {}".format(cpuPercentage))
-            print("Memory : {}/{} {}".format(memoryUsage, memoryLimit, memoryPercentage))
-            print("")
+    def updateStats(self, id, cpuPercentage, memoryUsage, memoryLimit, memoryPercentage, weight):
+        self.id = id
+        self.cpuPercentage = cpuPercentage
+        self.memoryUsage = memoryUsage
+        self.memoryLimit = memoryLimit
+        self.memoryPercentage = memoryPercentage       
+        self.weight = weight
 
-    def updateStats(self, id, cpuPercentage, memoryUsage, memoryLimit, memoryPercentage):
-        print("Name: {}".format(name))
-        print("id: {}".format(id))
-        print("CPU % : {}".format(cpuPercentage))
-        print("Memory : {}/{} {}".format(memoryUsage, memoryLimit, memoryPercentage))
+        print("Name: {}".format(self.name))
+        # print("id: {}".format(id)) 
+        print("CPU % : {}%".format(cpuPercentage))
+        # print("Memory : {}/{} {}%".format(memoryUsage, memoryLimit, memoryPercentage))
+        print("CPU Weight : {}".format(self.weight))
         print("")
 
-edgeRouter = MicroService("docker-compose_edge-router_1")
-frontend = MicroService("docker-compose_front-end_1")
-catalogue = MicroService("docker-compose_catalogue_1")
-carts = MicroService("docker-compose_carts_1")
-user = MicroService("docker-compose_user_1")
+        self.updateWeight()
 
-orders = MicroService("docker-compose_orders_1")
-shipping = MicroService("docker-compose_shipping_1")
-payment = MicroService("docker-compose_payment_1")
-queueMaster = MicroService("docker-compose_queue-master_1")
+    def updateWeight(self):
+        subprocess.check_output("docker update {} --cpu-shares={}".format(self.name, int(self.weight)), shell=False)
 
-rabbitmq = MicroService("docker-compose_rabbitmq_1")
-catalogueDB = MicroService("docker-compose_catalogue-db_1")
-cartsDB = MicroService("docker-compose_carts-db_1")
-userDB = MicroService("docker-compose_user-db_1")
-ordersDB = MicroService("docker-compose_orders-db_1")
+edgeRouter = Container("docker-compose_edge-router_1")
+frontend = Container("docker-compose_front-end_1")
+catalogue = Container("docker-compose_catalogue_1")
+carts = Container("docker-compose_carts_1")
+user = Container("docker-compose_user_1")
+
+orders = Container("docker-compose_orders_1")
+shipping = Container("docker-compose_shipping_1")
+payment = Container("docker-compose_payment_1")
+queueMaster = Container("docker-compose_queue-master_1")
+
+rabbitmq = Container("docker-compose_rabbitmq_1")
+catalogueDB = Container("docker-compose_catalogue-db_1")
+cartsDB = Container("docker-compose_carts-db_1")
+userDB = Container("docker-compose_user-db_1")
+ordersDB = Container("docker-compose_orders-db_1")
 
 dynamicAllocationTargets =[
     edgeRouter,
@@ -61,14 +68,24 @@ dynamicAllocationTargets =[
     ordersDB
 ]
 
+cpuMinWeight = 64
+cpuMaxWeight = 4096
+
 while 1:
     print("--------------------------------------------")
 
     lines = os.popen("docker stats --no-stream").readlines()[1:]
 
     infos = {}
+
+    cpuPercentageSum = 0
+    cpuWeightSum = 0
     for line in lines:
         id, name, cpuPercentage, memoryUsage, _, memoryLimit, memoryPercentage, netIn, _, netOut, blockIn, _, blockOut, pids = line.split()
+
+        cpuPercentage = float(cpuPercentage[:-1])
+        memoryPercentage = float(memoryPercentage[:-1])
+
         infos[name] = {
             "id": id,
             "name": name,
@@ -77,16 +94,20 @@ while 1:
             "memoryLimit": memoryLimit,
             "memoryPercentage": memoryPercentage
         }
+
+        cpuPercentageSum += cpuPercentage
+        cpuWeightSum += 1024
     
     for target in dynamicAllocationTargets:
-        name = target.containerName
+        name = target.name
         id = infos[name]["id"]
         cpuPercentage = infos[name]["cpuPercentage"]
         memoryUsage = infos[name]["memoryUsage"]
         memoryLimit = infos[name]["memoryLimit"]
         memoryPercentage = infos[name]["memoryPercentage"]
+        weight = max(min((cpuPercentage / cpuPercentageSum) * cpuWeightSum, cpuMaxWeight), cpuMinWeight)
 
-        target.updateStats(id, cpuPercentage, memoryUsage, memoryLimit, memoryPercentage)
+        target.updateStats(id, cpuPercentage, memoryUsage, memoryLimit, memoryPercentage, weight)
 
     print("--------------------------------------------")
 
